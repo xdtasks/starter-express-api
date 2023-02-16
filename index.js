4651ec51c3bd387955d59061176078f181793cf5
xdtasks <125533504+xdtasks@users.noreply.github.com>
2023-02-16 10:58:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,4 +4,8 @@ app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
+app.get('/',(req, res) => {
+    console.log("hii")
+    res.send("hey")
+})
 app.listen(process.env.PORT || 3000)
